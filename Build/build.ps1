082d064b4ed6104cca2e3e2849dea7cc92f94375
Sam Harwell <sam.harwell@rackspace.com>
2014-08-15 15:34:53 -0500

    Use unique signing keys for each target framework, validate keys during build

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,5 +1,7 @@
 param (
-	[switch]$Debug
+	[switch]$Debug,
+	[string]$VisualStudioVersion = "12.0",
+	[switch]$SkipKeyCheck
 )
 
 # build the solution
@@ -7,7 +9,7 @@ $SolutionPath = "..\UriTemplate.sln"
 
 # make sure the script was run from the expected path
 if (!(Test-Path $SolutionPath)) {
-	echo "The script was run from an invalid working directory."
+	$host.ui.WriteErrorLine('The script was run from an invalid working directory.')
 	exit 1
 }
 
@@ -22,14 +24,28 @@ If ($Debug) {
 # build the main project
 $msbuild = "$env:windir\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 
-&$msbuild '/nologo' '/m' '/nr:false' '/t:rebuild' "/p:Configuration=$BuildConfig" $SolutionPath
+&$msbuild '/nologo' '/m' '/nr:false' '/t:rebuild' "/p:Configuration=$BuildConfig" "/p:VisualStudioVersion=$VisualStudioVersion" $SolutionPath
 if ($LASTEXITCODE -ne 0) {
-	echo "Build failed, aborting!"
+	$host.ui.WriteErrorLine('Build failed, aborting!')
 	exit $p.ExitCode
+}
+
+# By default, do not create a NuGet package unless the expected strong name key files were used
+if (-not $SkipKeyCheck) {
+	. .\keys.ps1
+
+	foreach ($pair in $Keys.GetEnumerator()) {
+		$assembly = Resolve-FullPath -Path "..\Rackspace.Net.UriTemplate\bin\$($pair.Key)\$BuildConfig\Rackspace.Net.UriTemplate.dll"
+		# Run the actual check in a separate process or the current process will keep the assembly file locked
+		powershell -Command ".\check-key.ps1 -Assembly '$assembly' -ExpectedKey '$($pair.Value)' -Build '$($pair.Key)'"
+		if ($LASTEXITCODE -ne 0) {
+			Exit $p.ExitCode
+		}
+	}
 }
 
 if (-not (Test-Path 'nuget')) {
 	mkdir "nuget"
 }
 
-..\.nuget\NuGet.exe pack ..\Rackspace.Net.UriTemplate\Rackspace.Net.UriTemplate.nuspec -OutputDirectory nuget -Prop Configuration=$BuildConfig -Version $Version -Symbols
+..\.nuget\NuGet.exe 'pack' '..\Rackspace.Net.UriTemplate\Rackspace.Net.UriTemplate.nuspec' '-OutputDirectory' 'nuget' '-Prop' "Configuration=$BuildConfig" '-Version' "$Version" '-Symbols'
