67b3d78fd0a2f8008651b22ed203fce627de5292
Sam Harwell <sam.harwell@rackspace.com>
2014-08-20 06:19:10 -0500

    Move UriTemplate.DefaultRegexOptions to InternalRegexOptions.Default

---
+++ Rackspace.Net.UriTemplate/InternalRegexOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace Rackspace.Net
+{
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// This utility class determines the correct set of <see cref="RegexOptions"/> to specify when creating
+    /// <see cref="Regex"/> instances at runtime within this library.
+    /// </summary>
+    internal static class InternalRegexOptions
+    {
+#if !PORTABLE
+        /// <summary>
+        /// The default <see cref="RegexOptions"/> for non-Portable Class Library builds.
+        /// </summary>
+        internal const RegexOptions Default = RegexOptions.Compiled | RegexOptions.CultureInvariant;
+#else
+        /// <summary>
+        /// The default <see cref="RegexOptions"/> for Portable Class Library builds.
+        /// </summary>
+        internal const RegexOptions Default = RegexOptions.CultureInvariant;
+#endif
+    }
+}
