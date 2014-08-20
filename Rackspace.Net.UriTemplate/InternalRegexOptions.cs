d77692174844d80abfd5e8a7777f20c02dffbcaf
Sam Harwell <sam.harwell@rackspace.com>
2014-08-20 06:19:58 -0500

    Detect runtime support for RegexOptions.Compiled in PCL builds (fixes #50)

--- Rackspace.Net.UriTemplate/InternalRegexOptions.cs
+++ Rackspace.Net.UriTemplate/InternalRegexOptions.cs
@@ -2,22 +2,87 @@
 {
     using System.Text.RegularExpressions;
 
+#if PORTABLE
+    using System;
+#endif
+
     /// <summary>
     /// This utility class determines the correct set of <see cref="RegexOptions"/> to specify when creating
     /// <see cref="Regex"/> instances at runtime within this library.
     /// </summary>
     internal static class InternalRegexOptions
     {
+#if PORTABLE
+        /// <summary>
+        /// This is the backing field for the <see cref="Default"/> property.
+        /// </summary>
+        private static RegexOptions? _default;
+
+        /// <see href="http://referencesource.microsoft.com/#System/regex/system/text/regularexpressions/RegexOptions.cs#40">RegexOptions.Compiled</see>
+        private const RegexOptions RegexCompiledOption = (RegexOptions)0x0008;
+
+        /// <summary>
+        /// These options are preferred for any runtime which supports the combination.
+        /// </summary>
+        private const RegexOptions PreferredOptions = RegexOptions.CultureInvariant | RegexCompiledOption;
+
+        /// <summary>
+        /// These options are not preferred, but they are a safe fall-back for any runtime which does not support
+        /// <see cref="F:System.Text.RegularExpressions.RegexOptions.Compiled"/>.
+        /// </summary>
+        private const RegexOptions FallbackOptions = RegexOptions.CultureInvariant;
+#endif
+
+        /// <summary>
+        /// Gets the default <see cref="RegexOptions"/> to use when creating new <see cref="Regex"/> instances.
+        /// </summary>
+        /// <remarks>
+        /// <para>When targeting the Portable Class Library, the
+        /// <see cref="F:System.Text.RegularExpressions.RegexOptions.Compiled"/> option is not always available. It
+        /// does, however, have a consistent value when it is supported. The default options returned by this method
+        /// include <see cref="RegexOptions.CultureInvariant"/> and, when supported,
+        /// <see cref="F:System.Text.RegularExpressions.RegexOptions.Compiled"/>.</para>
+        /// </remarks>
+        public static RegexOptions Default
+        {
+            get
+            {
 #if !PORTABLE
-        /// <summary>
-        /// The default <see cref="RegexOptions"/> for non-Portable Class Library builds.
-        /// </summary>
-        internal const RegexOptions Default = RegexOptions.Compiled | RegexOptions.CultureInvariant;
+                return RegexOptions.CultureInvariant | RegexOptions.Compiled;
 #else
+                RegexOptions? defaultOptions = _default;
+                if (!defaultOptions.HasValue)
+                {
+                    defaultOptions = GetDefaultOptions();
+                    _default = defaultOptions;
+                }
+
+                return defaultOptions.Value;
+#endif
+            }
+        }
+
+#if PORTABLE
         /// <summary>
-        /// The default <see cref="RegexOptions"/> for Portable Class Library builds.
+        /// This method tests the current runtime to determine which set of default <see cref="RegexOptions"/> to use.
         /// </summary>
-        internal const RegexOptions Default = RegexOptions.CultureInvariant;
+        private static RegexOptions GetDefaultOptions()
+        {
+            RegexOptions options = RegexOptions.CultureInvariant | RegexCompiledOption;
+            try
+            {
+                Regex testExpression = new Regex("^x$", options);
+                return testExpression.IsMatch("x") ? options : FallbackOptions;
+            }
+            catch (ArgumentException)
+            {
+                return FallbackOptions;
+            }
+            catch (NotSupportedException)
+            {
+                return FallbackOptions;
+            }
+        }
 #endif
     }
 }
