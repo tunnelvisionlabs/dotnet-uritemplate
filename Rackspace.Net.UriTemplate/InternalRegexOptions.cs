69fce36b073ed21ecb4b2d7d89e00977e3495ff3
Sam Harwell <sam@tunnelvisionlabs.com>
2014-12-09 06:19:21 -0600

    Simplify InternalRegexOptions using Enum.TryParse

--- Rackspace.Net.UriTemplate/InternalRegexOptions.cs
+++ Rackspace.Net.UriTemplate/InternalRegexOptions.cs
@@ -10,28 +10,20 @@
     /// This utility class determines the correct set of <see cref="RegexOptions"/> to specify when creating
     /// <see cref="Regex"/> instances at runtime within this library.
     /// </summary>
+    /// <threadsafety static="true" instance="false"/>
     internal static class InternalRegexOptions
     {
+        private static readonly RegexOptions RegexCompiledOption;
+
+        static InternalRegexOptions()
+        {
 #if PORTABLE
-        /// <summary>
-        /// This is the backing field for the <see cref="Default"/> property.
-        /// </summary>
-        private static RegexOptions? _default;
-
-        /// <see href="http://referencesource.microsoft.com/#System/regex/system/text/regularexpressions/RegexOptions.cs#40">RegexOptions.Compiled</see>
-        private const RegexOptions RegexCompiledOption = (RegexOptions)0x0008;
-
-        /// <summary>
-        /// These options are preferred for any runtime which supports the combination.
-        /// </summary>
-        private const RegexOptions PreferredOptions = RegexOptions.CultureInvariant | RegexCompiledOption;
-
-        /// <summary>
-        /// These options are not preferred, but they are a safe fall-back for any runtime which does not support
-        /// <see cref="F:System.Text.RegularExpressions.RegexOptions.Compiled"/>.
-        /// </summary>
-        private const RegexOptions FallbackOptions = RegexOptions.CultureInvariant;
+            if (!Enum.TryParse("Compiled", out RegexCompiledOption))
+                RegexCompiledOption = RegexOptions.None;
+#else
+            RegexCompiledOption = RegexOptions.Compiled;
 #endif
+        }
 
         /// <summary>
         /// Gets the default <see cref="RegexOptions"/> to use when creating new <see cref="Regex"/> instances.
@@ -47,42 +39,8 @@
         {
             get
             {
-#if !PORTABLE
-                return RegexOptions.CultureInvariant | RegexOptions.Compiled;
-#else
-                RegexOptions? defaultOptions = _default;
-                if (!defaultOptions.HasValue)
-                {
-                    defaultOptions = GetDefaultOptions();
-                    _default = defaultOptions;
-                }
-
-                return defaultOptions.Value;
-#endif
+                return RegexOptions.CultureInvariant | RegexCompiledOption;
             }
         }
-
-#if PORTABLE
-        /// <summary>
-        /// This method tests the current runtime to determine which set of default <see cref="RegexOptions"/> to use.
-        /// </summary>
-        private static RegexOptions GetDefaultOptions()
-        {
-            RegexOptions options = RegexOptions.CultureInvariant | RegexCompiledOption;
-            try
-            {
-                Regex testExpression = new Regex("^x$", options);
-                return testExpression.IsMatch("x") ? options : FallbackOptions;
-            }
-            catch (ArgumentException)
-            {
-                return FallbackOptions;
-            }
-            catch (NotSupportedException)
-            {
-                return FallbackOptions;
-            }
-        }
-#endif
     }
 }
