4ba49bcf41118e6a10695cae00f2c2c7b6700e6b
Sam Harwell <sam@tunnelvisionlabs.com>
2014-12-09 06:41:10 -0600

    Remove the net40-client build (no advantage over the portable-net40 build) (closes #49)

--- Build/keys.ps1
+++ Build/keys.ps1
@@ -1,7 +1,6 @@
 # Note: these values may only change during minor release
 $Keys = @{
 	'net35-client' = '5d67c22833d5111b'
-	'net40-client' = 'f681c34e4e0427fa'
 	'portable-net40' = 'd56958160335d686'
 }
 
