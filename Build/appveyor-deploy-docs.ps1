f04b1d56e7f14fdc7ab1f362b056e8a7db084c49
Sam Harwell <sam@tunnelvisionlabs.com>
2015-09-02 23:24:25 -0500

    Updated AppVeyor build to include full tests and documentation output

---
+++ Build/appveyor-deploy-docs.ps1
@@ -0,0 +1,17 @@
+$DocsPath = "..\Docs.UriTemplate\bin\Help\portable-net40"
+
+# make sure the script was run from the expected path
+If (!(Test-Path $DocsPath)) {
+	$host.ui.WriteErrorLine('The script was run from an invalid working directory.')
+	Exit 1
+}
+
+function Upload-Folder() {
+	param([string]$LocalPath, [string]$ArtifactName)
+
+	[Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
+	[System.IO.Compression.ZipFile]::CreateFromDirectory((Join-Path (pwd) $LocalPath), (Join-Path (pwd) $ArtifactName))
+	Push-AppveyorArtifact (Join-Path (pwd) $ArtifactName)
+}
+
+Upload-Folder -LocalPath "$DocsPath" -ArtifactName "docs.zip"
