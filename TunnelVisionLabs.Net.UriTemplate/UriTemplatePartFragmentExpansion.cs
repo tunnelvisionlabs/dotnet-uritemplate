95e451b292c3c6b13461af4f884f41d041cb18e4
Sam Harwell <sam@tunnelvisionlabs.com>
2015-09-07 21:11:21 -0500

    Fix all violations of SA1405 (DebugAssertMustProvideMessageText)

--- TunnelVisionLabs.Net.UriTemplate/UriTemplatePartFragmentExpansion.cs
+++ TunnelVisionLabs.Net.UriTemplate/UriTemplatePartFragmentExpansion.cs
@@ -264,7 +264,7 @@ namespace TunnelVisionLabs.Net
                 Group mapKeys = match.Groups["var" + i + "key"];
                 if (mapKeys.Success && mapKeys.Captures.Count > 0)
                 {
-                    Debug.Assert(considerMap);
+                    Debug.Assert(considerMap, "considerMap");
                     Group mapValues = match.Groups["var" + i + "value"];
                     Dictionary<string, string> map = new Dictionary<string, string>();
                     for (int j = 0; j < mapKeys.Captures.Count; j++)
@@ -277,7 +277,7 @@ namespace TunnelVisionLabs.Net
                 // next try an array
                 if (!considerString || group.Captures.Count > 1)
                 {
-                    Debug.Assert(considerArray);
+                    Debug.Assert(considerArray, "considerArray");
                     List<string> list = new List<string>(group.Captures.Count);
                     foreach (Capture capture in group.Captures)
                         list.Add(DecodeCharacters(capture.Value));
@@ -286,7 +286,7 @@ namespace TunnelVisionLabs.Net
                     continue;
                 }
 
-                Debug.Assert(considerString);
+                Debug.Assert(considerString, "considerString");
                 results.Add(new KeyValuePair<VariableReference, object>(Variables[i], DecodeCharacters(group.Captures[0].Value)));
             }
 
