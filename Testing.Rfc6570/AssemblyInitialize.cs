4c6c84011d5f3434bfe0b0bd4c00b2f66e31b3ff
manuc66 <manuc66@gmail.com>
2015-11-03 23:53:10 +0100

    fix this error on with CultureInfo("fr-BE")
    Testing.Rfc6570.ExtendedTests.TestQueryExpansionWithMultipleDoubleVariable()
    dotnet-uritemplate\Testing.Rfc6570\ExtendedTests.cs:ligne 266
    Result Message: Assert.AreEqual failed. Expected:</loc?long=37.76&lat=-122.427>. Actual:</loc?long=37%2C76&lat=-122%2C427>.

---
+++ Testing.Rfc6570/AssemblyInitialize.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing.Rfc6570
+{
+  [TestClass]
+  public class AssemblyInitialize
+  {
+    [AssemblyInitialize]
+    public static void AssemblyInitializer(TestContext testContext)
+    {
+      Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+    }
+  }
+}
